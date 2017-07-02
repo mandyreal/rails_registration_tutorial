3c5c0dc9fc8e9d2bb67f8a4c8d7e54b303b8b6a9
Mandy Real <normandy.real@gmail.com>
2017-07-02 21:28:37 +0000

    Add test case to validate email address

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -9,4 +9,11 @@ class UserTest < ActiveSupport::TestCase
   end
 
   should validate_presence_of(:email)
+  
+  test "email should be a valid email address" do 
+    @subject.email = "invalid"
+    @subject.valid?
+    
+    assert_includes(@subject.errors[:email], "it is not an email")
+  end
 end
