5a509cf59d5c7a445911248371cf649ef7240b2f
Mandy Real <normandy.real@gmail.com>
2017-07-05 20:46:42 +1000

    Added Assignment model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170704161206) do
+ActiveRecord::Schema.define(version: 20170705104206) do
+
+  create_table "assignments", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "role_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["role_id"], name: "index_assignments_on_role_id"
+    t.index ["user_id"], name: "index_assignments_on_user_id"
+  end
 
   create_table "roles", force: :cascade do |t|
     t.string "name", null: false
